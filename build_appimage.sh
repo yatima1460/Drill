e56e419da46fbcecc94b749283fef31595dc500a
Federico Santamorena <federico@santamorena.me>
2019-05-03 07:04:31 +0200

    appimage files

--- build_appimage.sh
+++ build_appimage.sh
@@ -1,15 +1,15 @@
 #! /bin/bash
 
 rm *.AppImage
+rm -rf me.santamorena.drill
 echo Removed old AppImage files
-wget https://raw.githubusercontent.com/AppImage/pkg2appimage/master/pkg2appimage &> /dev/null
+wget https://raw.githubusercontent.com/AppImage/pkg2appimage/master/pkg2appimage
 echo Downloaded pkg2appimage script
 echo Building AppImage...
-bash pkg2appimage drill.yml &> /dev/null
+bash pkg2appimage drill.yml
 cd out
 mv *.AppImage ..
 cd ..
 rmdir out
-rm -rf me.santamorena.drill &> /dev/null
 rm pkg2appimage
 echo AppImage built
