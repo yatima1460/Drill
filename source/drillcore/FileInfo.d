4c769df34dee119b49a34b7165eef1a787d3ca43
Federico Santamorena <federico@santamorena.me>
2019-05-11 00:10:03 +0200

    some core functions

--- source/drillcore/FileInfo.d
+++ source/drillcore/FileInfo.d
@@ -1,22 +1,50 @@
+module drill.core.fileinfo;
 
-
-
-
-module DrillCore.Fileinfo;
-
-
-
-
-
-
-
+/**
+FileInfo is a struct filled by the information a crawler can find about a file
+*/
 struct FileInfo
 {
+    /***
+    true if the file is a directory
+    */
     bool isDirectory;
+
+    /***
+    true if the file is a normal file
+    */
+    bool isFile;
+
+    /***
+    true if is a symbolic link
+    */
+    bool isSymbolic;
+
+    /***
+    string of the date modified
+    */
     string dateModifiedString;
+
+    /*** 
+    the parent folder
+    */
     string containingFolder;
+
+    /***
+    name of the icon to use on the left of the name
+    */
     string iconName;
+
+    /***
+    the filename with extension
+    */
     string fileName;
+
+    /***
+    only the extension with the dot (ex: .png) 
+    */
+    string extension;
+
 }
 
 //     this(DirEntry de)
@@ -33,9 +61,6 @@ struct FileInfo
 //     {
 
 //     }
-
- 
-
 
 //     void openContainingFolder()
 //     {
@@ -54,15 +79,12 @@ struct FileInfo
 
 //     string getFileName()
 //     {
-       
-//     }
 
+//     }
 
 //     string getIconName()
 //     {
-       
+
 //     }
-
-
 
 // }
