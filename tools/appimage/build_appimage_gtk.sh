355f04b3f93eb5b0686670b9b320811aeb16660b
Federico Santamorena <federico@santamorena.me>
2019-05-13 22:17:43 +0200

    sh update

--- tools/appimage/build_appimage_gtk.sh
+++ tools/appimage/build_appimage_gtk.sh
@@ -1,9 +1,5 @@
 #! /bin/bash
-
-echo Removed old AppImage files
 wget -c https://raw.githubusercontent.com/AppImage/pkg2appimage/master/pkg2appimage
-echo Downloaded pkg2appimage script
-echo Building AppImage...
 bash pkg2appimage drill_gtk.yml
 rm pkg2appimage
 cd out
