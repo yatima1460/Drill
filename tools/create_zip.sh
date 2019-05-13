b97560a166547dc8bde30c909eb6e47363015229
Federico Santamorena <federico@santamorena.me>
2019-05-14 01:07:22 +0200

    fix CLI appimage

--- tools/create_zip.sh
+++ tools/create_zip.sh
@@ -1,3 +1,3 @@
 #!bin/bash
 zip -r Drill-GTK-Debian-Executable.zip ../assets ../Drill-GTK ../DRILL_VERSION
-zip -r Drill-CLI-Debian-Executable.zip ../assets ../Drill-GTK ../DRILL_VERSION
+zip -r Drill-CLI-Debian-Executable.zip ../assets ../Drill-CLI ../DRILL_VERSION
