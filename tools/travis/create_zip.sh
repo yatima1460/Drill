e589063d75cde8c229426b11d8217852fa245fd6
Federico Santamorena <federico@santamorena.me>
2019-05-14 18:50:35 +0200

    travis

--- tools/travis/create_zip.sh
+++ tools/travis/create_zip.sh
@@ -1,5 +1,2 @@
 #!bin/bash
-zip -r Drill-GTK-Debian-Executable.zip ../../assets ../../Drill-GTK ../../DRILL_VERSION
-zip -r Drill-CLI-Debian-Executable.zip ../../assets ../../Drill-CLI ../../DRILL_VERSION
-zip -r Drill-CLI-Windows.zip ../../assets ../../Drill-CLI.exe ../../DRILL_VERSION
-zip -r Drill-GTK-Windows.zip ../../assets ../../Drill-GTK.exe ../../DRILL_VERSION
+
