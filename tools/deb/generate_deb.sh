adf25b59a9d3670ce7838af8101a3469776f3b21
Federico Santamorena <federico@santamorena.me>
2019-05-19 19:37:41 +0200

    fixing package name #21

--- tools/deb/generate_deb.sh
+++ tools/deb/generate_deb.sh
@@ -14,15 +14,15 @@ rm -rf DEBFILE
 
 # add binary
 mkdir -p DEBFILE/usr/bin
-cp drill DEBFILE/usr/bin
-chmod +x DEBFILE/usr/bin/drill
+cp drill-search DEBFILE/usr/bin
+chmod +x DEBFILE/usr/bin/drill-search
 
 # add drill data
-mkdir -p DEBFILE/opt/drill/
-cp ../../Drill-GTK DEBFILE/opt/drill/Drill-GTK
-cp -r ../../assets DEBFILE/opt/drill/
+mkdir -p DEBFILE/opt/drill-search/
+cp ../../Drill-GTK DEBFILE/opt/drill-search/Drill-GTK
+cp -r ../../assets DEBFILE/opt/drill-search/
 #chmod -R 700 DEBFILE/opt/drill
-chmod +x DEBFILE/opt/drill/Drill-GTK
+chmod +x DEBFILE/opt/drill-search/Drill-GTK
 
 
 #add deb metadata
@@ -30,7 +30,7 @@ mkdir DEBFILE/DEBIAN
 cp control DEBFILE/DEBIAN
 
 if [ -f ../../DRILL_VERSION ]; then
-    cp ../../DRILL_VERSION DEBFILE/opt/drill/
+    cp ../../DRILL_VERSION DEBFILE/opt/drill-search/
     echo Version: $(cat ../../DRILL_VERSION) >> DEBFILE/DEBIAN/control
     cat DEBFILE/DEBIAN/control
     echo Building .deb for version $(cat ../../DRILL_VERSION)
@@ -42,14 +42,14 @@ fi
 
 # add desktop file
 mkdir -p DEBFILE/usr/share/applications
-desktop-file-validate drill.desktop
-cp drill.desktop DEBFILE/usr/share/applications/
+desktop-file-validate drill-search.desktop
+cp drill-search.desktop DEBFILE/usr/share/applications/
 
 # add icon
-mkdir -p DEBFILE/usr/share/icons/drill
+mkdir -p DEBFILE/usr/share/icons/drill-search
 #mkdir -p DEBFILE/usr/share/app-install/icons/
 
-cp ../../assets/icon.png DEBFILE/usr/share/icons/drill/drill.png
+cp ../../assets/icon.png DEBFILE/usr/share/icons/drill-search/drill.png
 #cp ../../assets/icon.svg DEBFILE/usr/share/app-install/icons/drill.svg
 
 
