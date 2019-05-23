f7ad3175cc6a566372c3ffa858fa02718b30774b
Federico Santamorena <federico@santamorena.me>
2019-05-23 02:34:34 +0200

    disable windows for now because it's a mess

--- build.bash
+++ build.bash
@@ -248,10 +248,18 @@ fi
 #===== CREATE A PORTABLE .ZIP FILE
 package() {
     local EXE_NAME=drill-$1
-    if [[ $OS == "windows" ]]; then EXE_NAME=drill-$1.exe; fi
+
+    if [[ $OS == "windows" ]]; then 
+        EXE_NAME=drill-$1.exe; 
+        git clone https://github.com/yatima1460/GTK3-windows-32bit;
+        mv GTK3-windows-32bit/*.dll .;
+        if [[ $1 == "gtk" ]]; then
+            7z a -tzip ../../build/Drill-gtk-windows-$TRAVIS_TAG-x86.zip assets drill-gtk.exe DRILL_VERSION *.dll;
+        fi
+    fi
+
     
-    
-    if 7z a -tzip Drill-"$1"-"$OS"-"$DRILL_VERSION"-"$ARCH".zip assets "$EXE_NAME" DRILL_VERSION  $OUTPUT; then
+    if 7z a -tzip Drill-"$1"-"$OS"-"$DRILL_VERSION"-"$ARCH".zip assets "$EXE_NAME" DRILL_VERSION -mx9 -ms -mmt $OUTPUT; then
         info "Zipping of $1 done"
         
         if mv Drill-"$1"-"$OS"-"$DRILL_VERSION"-"$ARCH".zip build $OUTPUT; then
@@ -478,13 +486,3 @@ pipeline
 # TODO: windows build
 # wget http://downloads.dlang.org/nightlies/dmd-master/dmd.master.windows.7z;
 # 7z x dmd.master.windows.7z;
-# git clone https://github.com/yatima1460/GTK3-windows-32bit;
-# cd source/cli
-# ../../dmd2/windows/bin/dub build -b release --parallel --force --arch=x86
-# cd ../../
-# 7z a -tzip ../../build/Drill-cli-windows-$TRAVIS_TAG-x86.zip assets drill-cli.exe;
-# cd source/gtkui
-# ../../dmd2/windows/bin/dub build -b release --parallel --force --arch=x86;
-# cd ../../
-# mv GTK3-windows-32bit/*.dll .;
-# 7z a -tzip ../../build/Drill-gtk-windows-$TRAVIS_TAG-x86.zip assets drill-gtk.exe DRILL_VERSION *.dll;
