038334365cb2539c8514a6d2264bd80773821ba0
Federico Santamorena <federico@santamorena.me>
2019-06-11 23:30:14 +0200

    file exists check

--- Tools/PortableZip/create_zip.bash
+++ Tools/PortableZip/create_zip.bash
@@ -9,8 +9,13 @@ FILES="../../Source/Frontend/GTK/Build/*
 for f in $FILES
 do
   echo "Processing $f file..."
-  7z a -tzip $f.zip $f
-  
-  mv $f.zip Build
+
+  if [[ -d "$f" ]]; then
+    echo "$f exists"
+    7z a -tzip $f.zip $f
+    mv $f.zip Build
+  else
+    echo "$f does not exist, skipping"
+  fi
   
 done
