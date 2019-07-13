d387521337a124b318b94ed6bb591a6e09636310
Federico Santamorena <federico@santamorena.me>
2019-07-14 00:51:53 +0200

    somewhat working webview

--- Source/Frontend/WebView/DWebView.d
+++ Source/Frontend/WebView/DWebView.d
@@ -1,6 +1,6 @@
 module WebView;
 
-import core.stdc.stdio :printf;
+import core.stdc.stdio : printf;
 
 import std.array : join;
 import std.stdio : writeln, readln;
@@ -11,53 +11,77 @@ import API : DrillAPI;
 import DhanosInterface : DhanosInterface;
 import Dhanos : getNewPlatformInstance;
 
+void drill_exit(DhanosInterface d, immutable(string) value)
+{
+    writeln("[Drill] drill_exit js callback");
 
+    // writeln("DHANOS_PTR");
+    writeln(d);
 
+    d.close();
+    // writeln("drill_exit end"); 
+}
 
- void drill_exit(DhanosInterface d, immutable(string) value)
-    {
-        writeln("[Drill] drill_exit js callback");
-     
-        // writeln("DHANOS_PTR");
-         writeln(d);
+void search(DhanosInterface d, immutable(string) value)
+{
+    writeln("[Drill] search");
 
-        
+    // writeln("DHANOS_PTR");
+    writeln(value);
 
-         d.close();   
-        // writeln("drill_exit end"); 
-    }
+    //d.close();   
+    // writeln("drill_exit end"); 
+}
 
+import ApplicationInfo : ApplicationInfo;
+
+void dhanos_page_loaded(DhanosInterface d, immutable(string) value)
+{
 
     
- void search(DhanosInterface d, immutable(string) value)
+    ApplicationInfo[] applications = DrillAPI.getApplicationsInfo();
+
+    int i = 0;
+    foreach (ApplicationInfo app; applications)
     {
-        writeln("[Drill] search");
-     
-        // writeln("DHANOS_PTR");
-         writeln(value);
-
-        
-
-         //d.close();   
-        // writeln("drill_exit end"); 
+        if (i == 10) break;
+        import std.uni : toLower;
+        d.runJavascript("addApplication(
+            \""~app.icon~"\",
+            \""~app.name~"\",
+            \""~app.exec~"\",
+            \""~app.desktopFileDateModifiedString~"\"
+            )");
+        i++;
+        //appendApplication(d,cast(immutable(ApplicationInfo))app);
     }
+}
 
-
+void appendApplication(DhanosInterface d, immutable(ApplicationInfo) a)
+{
+    d.runJavascript("javascript:addApplication("~a.name~");");
+}
 
 int main(string[] args)
 {
-    writeln("Drill WebView v"~DrillAPI.DRILL_VERSION~" - "~DrillAPI.GITHUB_URL);
+    writeln("Drill WebView v" ~ DrillAPI.DRILL_VERSION ~ " - " ~ DrillAPI.GITHUB_URL);
     immutable(string) title = "Drill";
-        immutable(string) dhanos_project_path = dirName(absolutePath(buildNormalizedPath(args[0])));
-        immutable(string) url = buildPath("file:" ~ dhanos_project_path ~ "/drill.html");
-        immutable int width = 800;
-        immutable int height = 250;
-        immutable bool resizable = false;
-         DhanosInterface d = getNewPlatformInstance(title, url, width, height, resizable);
-    
-        d.setCallback("exit",&drill_exit);
-         d.setCallback("search",&search);
-        d.setBorder(false);
-        d.mainLoop();
-       return 0;
+    immutable(string) dhanos_project_path = dirName(absolutePath(buildNormalizedPath(args[0])));
+    immutable(string) url = buildPath("file:" ~ dhanos_project_path ~ "/drill.html");
+    immutable int width = 800;
+    immutable int height = 250;
+    immutable bool resizable = false;
+
+    DhanosInterface d = getNewPlatformInstance(title, url, width, height, resizable);
+
+    immutable(string) assetsFolder = buildPath(absolutePath(dirName(buildNormalizedPath(args[0]))), "Assets");
+    DrillAPI drillapi = new DrillAPI(assetsFolder);
+
+   
+    d.setCallback("loaded",&dhanos_page_loaded);
+    d.setCallback("exit", &drill_exit);
+    d.setCallback("search", &search);
+    d.setBorder(false);
+    d.mainLoop();
+    return 0;
 }
