09cb1f13d86b39a6ca45840ed39a0130547848c7
Federico Santamorena <federico@santamorena.me>
2019-07-13 22:44:16 +0200

    fixed linker issues

--- Source/Frontend/WebView/DWebView.d
+++ Source/Frontend/WebView/DWebView.d
@@ -1,52 +1,46 @@
 module WebView;
 
-import std.array : join;
-import std.stdio : writeln, readln;
-
 import core.stdc.stdio :printf;
 
-// import FileInfo : FileInfo;
- import API : DrillAPI;
-// import Crawler : Crawler;
+import std.array : join;
+import std.stdio : writeln, readln;
 import std.path : buildPath;
-
-
-
-
-
 import std.path : dirName, buildNormalizedPath, absolutePath, buildPath;
 
+import API : DrillAPI;
 import DhanosInterface : DhanosInterface;
 import Dhanos : getNewPlatformInstance;
 
-void callback(immutable(string) value)
-{
-    writeln("borderless js callback");
-    writeln("value "~value);
-    
-}
 
 
+ void exit(DhanosInterface* d, immutable(string) value)
+    {
+        writeln("[Drill] drill_exit js callback");
+     
+        // writeln("DHANOS_PTR");
+         writeln(d);
+
+         auto dd = *d;
+          writeln(dd);
+
+         dd.close();   
+        // writeln("drill_exit end"); 
+    }
 
 
 int main(string[] args)
 {
     writeln("Drill WebView v"~DrillAPI.DRILL_VERSION~" - "~DrillAPI.GITHUB_URL);
-   //webview("Minimal webview example","https://en.m.wikipedia.org/wiki/Main_Page", 800, 600, 1);
-     //printf("%d",webview("Minimal webview example","file://drill.html", 800, 600, 1));
- immutable(string) title = "Drill";
-    immutable(string) dhanos_project_path = dirName(absolutePath(buildNormalizedPath(args[0])));
-    immutable(string) url = buildPath("file:" ~ dhanos_project_path ~ "/drill.html");
-    immutable int width = 800;
-    immutable int height = 250;
-    immutable bool resizable = false;
-    DhanosInterface d = getNewPlatformInstance(title, url, width, height, resizable);
+    immutable(string) title = "Drill";
+        immutable(string) dhanos_project_path = dirName(absolutePath(buildNormalizedPath(args[0])));
+        immutable(string) url = buildPath("file:" ~ dhanos_project_path ~ "/drill.html");
+        immutable int width = 800;
+        immutable int height = 250;
+        immutable bool resizable = false;
+         DhanosInterface d = getNewPlatformInstance(title, url, width, height, resizable);
     
-    //md.setBorder(false);
-    auto f = &callback;
-    d.setJSCallback(f);
-    d.mainLoop();
-    
-       
+        d.setCallback("exit",&exit);
+        //d.setBorder(false);
+        d.mainLoop();
     return 0;
 }
