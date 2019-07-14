ab5065069296f59b48e4dad671e208c6b908c7e0
Federico Santamorena <federico@santamorena.me>
2019-07-14 15:18:01 +0200

    screen width test

--- Source/Frontend/WebView/DWebView.d
+++ Source/Frontend/WebView/DWebView.d
@@ -35,7 +35,6 @@ void search(DhanosInterface d, immutable(string) value)
 
 import ApplicationInfo : ApplicationInfo;
 
-
 void drill_return(DhanosInterface d, immutable(string) value)
 {
 
@@ -45,38 +44,42 @@ void drill_return(DhanosInterface d, immutable(string) value)
 void dhanos_page_loaded(DhanosInterface d, immutable(string) value)
 {
 
-    
     ApplicationInfo[] applications = DrillAPI.getApplicationsInfo();
 
     int i = 0;
     foreach (ApplicationInfo app; applications)
     {
-        if (i == 10) break;
+        if (i == 10)
+            break;
         import std.uni : toLower;
+
         d.runJavascript("addApplication(
-            \""~app.icon~"\",
-            \""~app.name~"\",
-            \""~app.exec~"\",
-            \""~app.desktopFileDateModifiedString~"\"
+            \"" ~ app.icon ~ "\",
+            \"" ~ app.name ~ "\",
+            \""
+                ~ app.exec ~ "\",
+            \"" ~ app.desktopFileDateModifiedString ~ "\"
             )");
         i++;
         //appendApplication(d,cast(immutable(ApplicationInfo))app);
     }
 }
 
-
-
 void open_drill_website(DhanosInterface d, immutable(string) value)
 {
     writeln("open_drill_website");
     import Utils : openFile;
+
     openFile(DrillAPI.WEBSITE_URL);
 }
 
 void appendApplication(DhanosInterface d, immutable(ApplicationInfo) a)
 {
-    d.runJavascript("javascript:addApplication("~a.name~");");
+    d.runJavascript("javascript:addApplication(" ~ a.name ~ ");");
 }
+
+extern (C) int gdk_screen_width();
+extern (C) int gdk_screen_height();
 
 int main(string[] args)
 {
@@ -85,25 +88,28 @@ int main(string[] args)
     immutable(string) dhanos_project_path = dirName(absolutePath(buildNormalizedPath(args[0])));
     immutable(string) url = buildPath("file:" ~ dhanos_project_path ~ "/drill.html");
     immutable int width = 960;
-    immutable int height = 80;
+    immutable int height = 78;
     // width = 100;
     // height = 100;
     immutable bool resizable = false;
 
     DhanosInterface d = getNewPlatformInstance(title, url, width, height, resizable);
 
-    immutable(string) assetsFolder = buildPath(absolutePath(dirName(buildNormalizedPath(args[0]))), "Assets");
+    //d.setWindowSize(gdk_screen_width() / 2, cast(int)(gdk_screen_height() * 0.1f));
+
+    immutable(string) assetsFolder = buildPath(
+            absolutePath(dirName(buildNormalizedPath(args[0]))), "Assets");
     DrillAPI drillapi = new DrillAPI(assetsFolder);
 
-d.setBorder(false);
+    d.setBorder(false);
     d.setAlwaysOnTop(true);
-   
-    d.setCallback("loaded",&dhanos_page_loaded);
+
+    d.setCallback("loaded", &dhanos_page_loaded);
     d.setCallback("close", &drill_exit);
     d.setCallback("search", &search);
     d.setCallback("return", &drill_return);
     d.setCallback("open_drill_website", &open_drill_website);
-    
+
     d.mainLoop();
     return 0;
 }
