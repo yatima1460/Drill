31f1dbb01267c51e646c9e0dc966ea0745738427
Federico Santamorena <federico@santamorena.me>
2019-07-14 15:48:14 +0200

    some fixes

--- Source/Frontend/WebView/DWebView.d
+++ Source/Frontend/WebView/DWebView.d
@@ -8,63 +8,127 @@ import std.path : buildPath;
 import std.path : dirName, buildNormalizedPath, absolutePath, buildPath;
 
 import API : DrillAPI;
+import FileInfo : FileInfo;
+
 import DhanosInterface : DhanosInterface;
 import Dhanos : getNewPlatformInstance;
 
+extern (C) int gdk_screen_width();
+extern (C) int gdk_screen_height();
+
+
+
+/**
+    JS Callback: Closes Drill when the user presses ESC or Return when no input
+*/
 void drill_exit(DhanosInterface d, immutable(string) value)
 {
     writeln("[Drill] drill_exit js callback");
-
-    // writeln("DHANOS_PTR");
-    writeln(d);
-
     d.close();
-    // writeln("drill_exit end"); 
 }
 
-void search(DhanosInterface d, immutable(string) value)
+/**
+    DrillAPI callback called when Drill finds a new result
+*/
+void resultFound(immutable(FileInfo) result)
 {
+     writeln("[Drill] resultFound START");
+    synchronized
+    {
+            writeln(result.fileName);
+    }
+    
+
+     writeln("[Drill] resultFound END");
+    //list_dirty = true;
+    //(cast(DList!FileInfo)*buffer).insertFront(result);
+}
+
+/**
+    JS Callback: called when the user inputs something
+*/
+extern (D) void search(DhanosInterface d, immutable(string) value)
+{
+    assert(value !is null);
     writeln("[Drill] search");
 
     // writeln("DHANOS_PTR");
     writeln(value);
 
+    if (value == "")
+    {
+        writeln("[Drill] return to normal size");
+        d.setWindowSize(gdk_screen_width()/2,78);
+       
+    }
+    else
+    {
+        writeln("[Drill] set bigger window size");
+        d.setWindowSize(gdk_screen_width()/2,200);
+    }
+
+    auto drillapi = cast(DrillAPI)d.getUserObject();
+
+    assert(drillapi !is null);
+
+    writeln("[Drill] crawling stop");
+    drillapi.stopCrawlingAsync();
+        
+
+    import std.functional : toDelegate;
+    drillapi.startCrawling("%£da", toDelegate(&resultFound));
+
+
+    writeln("[Drill] search END");
+
+   
     //d.close();   
     // writeln("drill_exit end"); 
 }
 
 import ApplicationInfo : ApplicationInfo;
 
+
+/**
+    JS Callback: called when the user presses Return and the input is not empty
+*/
 void drill_return(DhanosInterface d, immutable(string) value)
 {
 
     writeln("return pressed");
 }
 
-void dhanos_page_loaded(DhanosInterface d, immutable(string) value)
-{
 
-    ApplicationInfo[] applications = DrillAPI.getApplicationsInfo();
+// /**
+//     JS Callback: called when the page loaded
+// */
+// void dhanos_page_loaded(DhanosInterface d, immutable(string) value)
+// {
 
-    int i = 0;
-    foreach (ApplicationInfo app; applications)
-    {
-        if (i == 10)
-            break;
-        import std.uni : toLower;
+//     ApplicationInfo[] applications = DrillAPI.getApplicationsInfo();
 
-        d.runJavascript("addApplication(
-            \"" ~ app.icon ~ "\",
-            \"" ~ app.name ~ "\",
-            \""
-                ~ app.exec ~ "\",
-            \"" ~ app.desktopFileDateModifiedString ~ "\"
-            )");
-        i++;
-        //appendApplication(d,cast(immutable(ApplicationInfo))app);
-    }
-}
+//     int i = 0;
+//     foreach (ApplicationInfo app; applications)
+//     {
+//         if (i == 10)
+//             break;
+//         import std.uni : toLower;
 
+//         d.runJavascript("addApplication(
+//             \"" ~ app.icon ~ "\",
+//             \"" ~ app.name ~ "\",
+//             \""
+//                 ~ app.exec ~ "\",
+//             \"" ~ app.desktopFileDateModifiedString ~ "\"
+//             )");
+//         i++;
+//         //appendApplication(d,cast(immutable(ApplicationInfo))app);
+//     }
+// }
+
+/**
+    JS Callback: Opens the Drill website when the user clicks on the logo
+*/
 void open_drill_website(DhanosInterface d, immutable(string) value)
 {
     writeln("open_drill_website");
@@ -73,13 +137,6 @@ void open_drill_website(DhanosInterface d, immutable(string) value)
     openFile(DrillAPI.WEBSITE_URL);
 }
 
-void appendApplication(DhanosInterface d, immutable(ApplicationInfo) a)
-{
-    d.runJavascript("javascript:addApplication(" ~ a.name ~ ");");
-}
-
-extern (C) int gdk_screen_width();
-extern (C) int gdk_screen_height();
 
 int main(string[] args)
 {
@@ -103,8 +160,9 @@ int main(string[] args)
 
     d.setBorder(false);
     d.setAlwaysOnTop(true);
+    d.setUserObject(drillapi);
 
-    d.setCallback("loaded", &dhanos_page_loaded);
+    //d.setCallback("loaded", &dhanos_page_loaded);
     d.setCallback("close", &drill_exit);
     d.setCallback("search", &search);
     d.setCallback("return", &drill_return);
