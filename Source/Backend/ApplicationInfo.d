bd3eacab03745235184cef956af7f10f1f7f1017
Federico Santamorena <federico@santamorena.me>
2019-07-19 17:27:37 +0200

    ApplicationInfo is now an immutable struct

--- Source/Backend/ApplicationInfo.d
+++ Source/Backend/ApplicationInfo.d
@@ -1,26 +1,52 @@
 module ApplicationInfo;
 
 
+/**
+Struct containing the data Drill found about installed applications
+*/
 struct ApplicationInfo
 {
 
-    string desktopFileFullPath;
+    /*
+    Name of the installed application
+    */
+    immutable(string) name;
 
-    string name;
+    /*
+    Location where the app is installed
+    */
+    // string installedLocation;
+    
+    version(linux)
+    {
+        /*
+        Path to the .desktop file (Linux only)
+        */
+        immutable(string) desktopFileFullPath;
+        
+        /*
+        Command line to execute it on the .desktop file
+        */
+        immutable(string) exec;
 
+        /*
+        Cleaned command line execution line
+        removed % and splitted by space
+        */
+        immutable(string[]) execProcess;
 
+        /*
+        Icon to use
+        */
+        immutable(string) icon = "application-x-executable";
 
-    string exec;
+        /*
+        Date modified of the .desktop file
+        */
+        immutable(string) desktopFileDateModifiedString;
+    }
+    // version(Windows)
+    // {
 
-    // removed % and splitted by space
-    string[] execProcess;
-
-    //string[] execProcessSplitted;
-
-    string icon = "application-x-executable";
-
-    string desktopFileDateModifiedString;
-
-
-
+    // }
 }
