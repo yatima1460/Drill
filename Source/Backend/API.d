24a9466e6284b1794cae1333c1376d36787cea96
Federico Santamorena <federico@santamorena.me>
2019-07-19 19:17:33 +0200

    some functional conversion

--- Source/Backend/API.d
+++ Source/Backend/API.d
@@ -1,7 +1,7 @@
 module API;
 
 import std.algorithm : canFind, filter, map;
-import std.container : Array;
+import std.container : Array, SList;
 import std.array : array, split;
 import std.process : executeShell;
 import std.string : indexOf;
@@ -14,6 +14,166 @@ import Utils : readListFiles;
 import Logger : Logger;
 import Crawler : Crawler;
 import FileInfo : FileInfo;
+
+immutable(string) DRILL_VERSION = import("DRILL_VERSION");
+immutable(string) DRILL_BUILD_TIME = __TIMESTAMP__;
+immutable(string) DRILL_GITHUB_URL = "https://github.com/yatima1460/Drill";
+immutable(string) DRILL_WEBSITE_URL = "https://www.drill.santamorena.me";
+immutable(string) DRILL_AUTHOR_URL = "https://www.linkedin.com/in/yatima1460/";
+immutable(string) DRILL_AUTHOR_NAME = "Federico Santamorena";
+
+struct drill_data
+{
+    immutable(string) ASSETS_DIRECTORY;
+    immutable(string[]) BLOCK_LIST;
+    immutable(string[]) PRIORITY_LIST;
+    const(Regex!char[]) PRIORITY_LIST_REGEX;
+    bool singlethread;
+}
+
+struct drill_context
+{
+    immutable(string) search_value;
+    SList!Crawler threads;
+}
+
+/**
+A crawler is active when it's scanning something.
+If a crawler cleanly finished its job it's not considered active.
+If a crawler crashes (should never happen) it's not considered active.
+Minimum: 0
+Maximum: length of total number of mountpoints unless the user started the crawlers manually
+
+Returns: number of crawlers active
+
+*/
+@nogc @safe immutable(uint) drill_active_crawlers_count(drill_context context)
+{
+    int active = 0;
+    foreach (thread; context.threads)
+    {
+        if (thread.isCrawling())
+            active++;
+    }
+    return active;
+}
+
+/*
+Notifies the crawlers to stop and clears the crawlers array stored inside DrillAPI
+This function is non-blocking.
+If no crawling is currently underway this function will do nothing.
+*/
+void drill_stop_crawling_async(drill_context context)
+{
+    foreach (Crawler crawler; context.threads)
+        crawler.stopAsync();
+    context.threads.clear(); // TODO: if nothing has a reference to a thread does the thread get GC-ed?
+}
+
+
+void drill_wait_for_crawlers(drill_context context)
+{
+    Logger.logInfo("Waiting for "~to!string(drill_active_crawlers_count(context))~" crawlers to stop");
+    foreach (Crawler crawler; context.threads)
+    {
+        Logger.logInfo("Waiting for crawler "~to!string(crawler)~" to stop");
+        import core.thread : ThreadException;
+        try
+        {
+            crawler.join();
+            Logger.logInfo("Crawler "~to!string(crawler)~" stopped");
+        }
+        catch(ThreadException e)
+        {
+            Logger.logError("Thread "~crawler.toString()~" crashed when joining");
+            Logger.logError(e.msg);
+        }
+        
+    }
+    Logger.logInfo("All crawlers stopped.");
+}
+
+/*
+
+*/
+void drill_stop_crawling_sync(drill_context context)
+{
+    foreach (Crawler crawler; context.threads)
+        crawler.stopAsync();
+    drill_wait_for_crawlers(context);
+}
+
+/*
+Starts the crawling returning a drill_context object
+*/
+drill_context drill_start_crawling(drill_data data, immutable(string) search_value, void function(immutable(FileInfo) result, void* user_object) result_callback, void* user_object)
+{
+    drill_context c = {search_value};
+    if (user_object is null)
+        throw new Exception("it does not make sense for a userObject to be null");
+    
+    foreach (immutable(string) mountpoint; DrillAPI.getMountPoints())
+    {
+        Crawler crawler = new Crawler(mountpoint, data.BLOCK_LIST, data.PRIORITY_LIST_REGEX, result_callback, search_value,user_object);
+        if (data.singlethread)
+            crawler.run();
+        else
+            crawler.start();
+        c.threads.insertFront(crawler);
+    }
+    return c;
+}
+
+/*
+Loads Drill data to be used in any crawling
+*/
+drill_data drill_load_data(immutable(string) assets_directory)
+{
+    Logger.logDebug("DrillAPI " ~ DRILL_VERSION);
+    Logger.logDebug("Mount points found: "~to!string(DrillAPI.getMountPoints()));
+    auto blockListsFullPath = buildPath(assets_directory,"BlockLists");
+
+
+    Logger.logDebug("Assets Directory: " ~ assets_directory);
+    Logger.logDebug("blockListsFullPath: " ~ blockListsFullPath);
+
+    string[] BLOCK_LIST; 
+
+    try
+    {
+        BLOCK_LIST = readListFiles(blockListsFullPath);
+    }
+    catch (FileException fe)
+    {
+        Logger.logError(fe.toString());
+        Logger.logError("Error when trying to load block lists, will default to an empty list");
+    }
+
+    string[] PRIORITY_LIST;
+    Regex!char[] PRIORITY_LIST_REGEX;
+
+    try
+    {
+        PRIORITY_LIST = readListFiles(buildPath(assets_directory,"PriorityLists"));
+        PRIORITY_LIST_REGEX = PRIORITY_LIST[].map!(x => regex(x)).array;
+    }
+    catch (FileException fe)
+    {
+        Logger.logError(fe.toString());
+        Logger.logError("Error when trying to read priority lists, will default to an empty list");
+    }
+
+    drill_data dd = 
+    {
+        assets_directory,
+        cast(immutable(string[]))BLOCK_LIST,
+        cast(immutable(string[]))PRIORITY_LIST,
+        PRIORITY_LIST_REGEX
+    };
+    return dd;
+}
+
+
 
 
 class DrillAPI
