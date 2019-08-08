dc07ae4880b2089f04346e456c52c8048e38bfce
Federico Santamorena <federico@santamorena.me>
2019-08-08 06:23:46 +0200

    CLI cleaner code rewrite

--- CLI/CLI.d
+++ CLI/CLI.d
@@ -1,50 +1,45 @@
-
-
-import core.stdc.stdlib : exit;
-import core.stdc.stdio :printf;
+import core.stdc.stdio : printf;
 
 import std.array : join;
-import std.stdio : writeln, readln;
+import std.stdio : writeln, readln, stderr;
+import std.path : dirName, buildNormalizedPath, absolutePath;
+import std.getopt : getopt, defaultGetoptPrinter, config, GetOptException;
+import std.file : thisExePath;
 
 import FileInfo : FileInfo;
-import Crawler : Crawler;
+import Crawler : Crawler, CrawlerCallback;
 import std.path : buildPath;
 import Config : DrillConfig, loadData;
-import Context : DrillContext, startCrawling;
+import Context : DrillContext, startCrawling, waitForCrawlers;
 import Meta : VERSION, GITHUB_URL;
 
 // TODO: capture Ctrl-C and close crawlers?
 
-import std.variant : Variant;
-void resultsFoundWithDate(const(FileInfo) result,  void*  userObject)
+void resultsFoundWithDate(const(FileInfo) result, void* userObject)
 {
     synchronized
     {
-        writeln(result.dateModifiedString,"\t",result.fullPath);
+        writeln(result.dateModifiedString, "\t", result.fullPath);
     }
 }
 
-
-void resultsFoundWithSize(const(FileInfo) result,  void*  userObject)
+void resultsFoundWithSize(const(FileInfo) result, void* userObject)
 {
     synchronized
     {
-        writeln(result.sizeString,"\t",result.fullPath);
+        writeln(result.sizeString, "\t", result.fullPath);
     }
 }
-
 
 void resultsFoundWithSizeAndDate(const(FileInfo) result, void* userObject)
 {
     synchronized
     {
-        writeln(result.dateModifiedString,"\t",result.sizeString,"\t",result.fullPath);
+        writeln(result.dateModifiedString, "\t", result.sizeString, "\t", result.fullPath);
     }
 }
 
-
-
-void resultsFoundBare(const(FileInfo) result,  void*  userObject)
+void resultsFoundBare(const(FileInfo) result, void* userObject)
 {
     synchronized
     {
@@ -53,72 +48,71 @@ void resultsFoundBare(const(FileInfo) result,  void*  userObject)
 }
 
 
-immutable(string) searchInput()
-{
-    printf("Search for: ");
-    string search = readln();
-    immutable(string) searchString = search[0 .. search.length-1];
-    if (searchString.length == 0)
-        return searchInput();
-    return searchString;
-}
-
-
 int main(string[] args)
 {
-    import std.path : dirName, buildNormalizedPath, absolutePath;
-    import std.getopt : getopt, defaultGetoptPrinter, config;
-
-    DrillConfig data = loadData(buildPath(absolutePath(dirName(buildNormalizedPath(args[0]))),"Assets"));
-
-    bool date = false;
-    bool size = false;
-
-    auto opt = getopt(args,
-        config.bundling,
-        config.passThrough,
-        "date|d", "Show results date", &date,
-        "size|s", "Show results size", &size
-    );
-
-    if(opt.helpWanted) 
+    try
     {
-        writeln("Drill CLI v"~VERSION~" - "~GITHUB_URL);
-        writeln("Example use: drill-cli -ds \"foobar\"");
-        defaultGetoptPrinter("Options:", opt.options);
-        import core.stdc.stdlib : exit;
-        exit(0);
-    }
+        DrillConfig data = loadData(buildPath(dirName(thisExePath), "Assets"));
 
-    switch (args.length)
-    {
-        // What even is POSIX
-        case 0:
-            writeln("Your operating system does not pass the executable path as first argument");
-            exit(-2);
-            break;
-            
-        // No search string
-        case 1:
-            writeln("Pass a string as an argument for Drill to search");
+        bool date = false;
+        bool size = false;
+
+
+        auto opt = getopt(
+            args, 
+            config.bundling, config.passThrough, 
+            "date|d", "Show results date", &date, 
+            "size|s", "Show results size", &size);
+
+        if (opt.helpWanted)
+        {
+            writeln("Drill CLI v" ~ VERSION ~ " - " ~ GITHUB_URL);
             writeln("Example use: drill-cli -ds \"foobar\"");
             defaultGetoptPrinter("Options:", opt.options);
-            import core.stdc.stdlib : exit;
-            exit(-1);
-            break;
+            return 0;
+        }
 
-        // Search string provided
-        case 2:
-            auto selectedPrint = (date ?
-                             (size ? &resultsFoundWithSizeAndDate : &resultsFoundWithDate)
-                             :(size ? &resultsFoundWithSize : &resultsFoundBare));
-            auto context = startCrawling(data,args[1],*&selectedPrint,null);
-            break;
+        switch (args.length)
+        {
 
-        // More unnecessary arguments
-        default:
-            writeln("Oops, you gave more arguments than expected.");
-            exit(-1);
+            // What even is POSIX
+            case 0:
+                stderr.writeln("Your operating system does not pass the executable path as first argument");
+                return 1;
+
+            // No search string
+            case 1:
+                writeln("Pass a string as an argument for Drill to search");
+                writeln("Example use: drill-cli -ds \"foobar\"");
+                defaultGetoptPrinter("Options:", opt.options);
+                return 0;
+
+            // Search string provided
+            case 2:
+                CrawlerCallback[bool][bool] printCallback;
+                printCallback[false][false] = &resultsFoundBare;
+                printCallback[false][true] = &resultsFoundWithDate;
+                printCallback[true][false] = &resultsFoundWithSize;
+                printCallback[true][true] = &resultsFoundWithSizeAndDate;
+                DrillContext context = startCrawling(data, args[1], printCallback[size][date], null);
+                context.threads.waitForCrawlers();
+                return 0;
+
+            // More unnecessary arguments
+            default:
+                stderr.writeln("Oops, you gave more arguments than expected.");
+                return 1;
+
+        }
     }
-    return 0;
+    catch (GetOptException e)
+    {
+        stderr.writefln("Error processing command line arguments: %s", e.msg);
+        return 1;
+    }
+    catch (Exception e)
+    {
+        stderr.writefln("Generic unknown error: %s", e.msg);
+        return 1;
+    }
 }
