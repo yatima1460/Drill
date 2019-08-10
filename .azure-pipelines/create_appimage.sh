b63dcfd7979bceffffb7f2e82a4870adce6c558a
Federico Santamorena <federico@santamorena.me>
2019-08-10 21:34:41 +0200

    appimage fix

--- .azure-pipelines/create_appimage.sh
+++ .azure-pipelines/create_appimage.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -e
-mkdir -p $(Pipeline.Workspace)/Drill.AppDir
-cp -R $(Pipeline.Workspace)/Drill-GTK-linux-x86_64-release/* $(Pipeline.Workspace)/Drill.AppDir
-mkdir -p $(Pipeline.Workspace)/Drill.AppDir/usr/share/metainfo
-mkdir -p $(Pipeline.Workspace)/Drill.AppDir/usr/share/applications
-cp $(Pipeline.Workspace)/Assets/GTK-Linux/drill.software.appdata.xml $(Pipeline.Workspace)/Drill.AppDir/usr/share/metainfo
-cp $(Pipeline.Workspace)/Assets/GTK-Linux/drill-search-gtk.desktop $(Pipeline.Workspace)/Drill.AppDir/usr/share/applications
-ln -s drill-search-gtk $(Pipeline.Workspace)/Drill.AppDir/AppRun
-cp $(Pipeline.Workspace)/Assets/GTK-Linux/drill-search-gtk.svg $(Pipeline.Workspace)/Drill.AppDir
-wget https://github.com/AppImage/AppImageKit/releases/download/12/appimagetool-x86_64.AppImage -O $(Pipeline.Workspace)/appimagetool-x86_64.AppImage
-chmod +x $(Pipeline.Workspace)/appimagetool-x86_64.AppImage
-export ARCH=x86_64 && $(Pipeline.Workspace)/appimagetool-x86_64.AppImage $(Pipeline.Workspace)/Drill.AppDir
-test -f $(Pipeline.Workspace)/Drill-x86_64.AppImage
+mkdir -p $PIPELINE_WORKSPACE/Drill.AppDir
+cp -R $PIPELINE_WORKSPACE/Drill-GTK-linux-x86_64-release/* $PIPELINE_WORKSPACE/Drill.AppDir
+mkdir -p $PIPELINE_WORKSPACE/Drill.AppDir/usr/share/metainfo
+mkdir -p $PIPELINE_WORKSPACE/Drill.AppDir/usr/share/applications
+cp $PIPELINE_WORKSPACE/Assets/GTK-Linux/drill.software.appdata.xml $PIPELINE_WORKSPACE/Drill.AppDir/usr/share/metainfo
+cp $PIPELINE_WORKSPACE/Assets/GTK-Linux/drill-search-gtk.desktop $PIPELINE_WORKSPACE/Drill.AppDir/usr/share/applications
+ln -s drill-search-gtk $PIPELINE_WORKSPACE/Drill.AppDir/AppRun
+cp $PIPELINE_WORKSPACE/Assets/GTK-Linux/drill-search-gtk.svg $PIPELINE_WORKSPACE/Drill.AppDir
+wget https://github.com/AppImage/AppImageKit/releases/download/12/appimagetool-x86_64.AppImage -O $PIPELINE_WORKSPACE/appimagetool-x86_64.AppImage
+chmod +x $PIPELINE_WORKSPACE/appimagetool-x86_64.AppImage
+export ARCH=x86_64 && $PIPELINE_WORKSPACE/appimagetool-x86_64.AppImage $PIPELINE_WORKSPACE/Drill.AppDir
+test -f $PIPELINE_WORKSPACE/Drill-x86_64.AppImage
