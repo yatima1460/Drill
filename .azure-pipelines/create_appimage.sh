29150743e1c025cf7c9b183881e9a3c1af9bebf2
Federico Santamorena <federico@santamorena.me>
2019-08-10 21:29:04 +0200

    appimage test

--- .azure-pipelines/create_appimage.sh
+++ .azure-pipelines/create_appimage.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -e
-mkdir -p Drill.AppDir
-cp -R Drill-GTK-linux-x86_64-release/* Drill.AppDir
-mkdir -p Drill.AppDir/usr/share/metainfo
-mkdir -p Drill.AppDir/usr/share/applications
-cp Assets/GTK-Linux/drill.software.appdata.xml Drill.AppDir/usr/share/metainfo
-cp Assets/GTK-Linux/drill-search-gtk.desktop Drill.AppDir/usr/share/applications
-ln -s drill-search-gtk Drill.AppDir/AppRun
-cp Assets/GTK-Linux/drill-search-gtk.svg Drill.AppDir
-wget https://github.com/AppImage/AppImageKit/releases/download/12/appimagetool-x86_64.AppImage
-chmod +x appimagetool-x86_64.AppImage
-export ARCH=x86_64 && ./appimagetool-x86_64.AppImage Drill.AppDir
-test -f Drill-x86_64.AppImage
+mkdir -p $(Pipeline.Workspace)/Drill.AppDir
+cp -R $(Pipeline.Workspace)/Drill-GTK-linux-x86_64-release/* $(Pipeline.Workspace)/Drill.AppDir
+mkdir -p $(Pipeline.Workspace)/Drill.AppDir/usr/share/metainfo
+mkdir -p $(Pipeline.Workspace)/Drill.AppDir/usr/share/applications
+cp $(Pipeline.Workspace)/Assets/GTK-Linux/drill.software.appdata.xml $(Pipeline.Workspace)/Drill.AppDir/usr/share/metainfo
+cp $(Pipeline.Workspace)/Assets/GTK-Linux/drill-search-gtk.desktop $(Pipeline.Workspace)/Drill.AppDir/usr/share/applications
+ln -s drill-search-gtk $(Pipeline.Workspace)/Drill.AppDir/AppRun
+cp $(Pipeline.Workspace)/Assets/GTK-Linux/drill-search-gtk.svg $(Pipeline.Workspace)/Drill.AppDir
+wget https://github.com/AppImage/AppImageKit/releases/download/12/appimagetool-x86_64.AppImage -O $(Pipeline.Workspace)/appimagetool-x86_64.AppImage
+chmod +x $(Pipeline.Workspace)/appimagetool-x86_64.AppImage
+export ARCH=x86_64 && $(Pipeline.Workspace)/appimagetool-x86_64.AppImage $(Pipeline.Workspace)/Drill.AppDir
+test -f $(Pipeline.Workspace)/Drill-x86_64.AppImage
