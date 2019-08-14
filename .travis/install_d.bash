18f401fba027282221db79c135dfd5ffcd46d994
Federico Santamorena <federico@santamorena.me>
2019-08-14 13:37:24 +0200

    dmd cache test

--- .travis/install_d.bash
+++ .travis/install_d.bash
@@ -2,8 +2,8 @@
 
 if [ "$TRAVIS_OS_NAME" = "linux" ]; then
     wget -nv http://downloads.dlang.org/releases/2.x/"$DMD_VERSION"/dmd."$DMD_VERSION".linux.tar.xz
-    tar -xf dmd."$DMD_VERSION".linux.tar.xz 1>/dev/null
-    export DUB_LOCATION=$PWD/dmd2/linux/bin64
+    tar -xf dmd."$DMD_VERSION".linux.tar.xz 1>/dev/null -C dmd."$DMD_VERSION"
+    export DUB_LOCATION=$PWD/dmd."$DMD_VERSION"/linux/bin64
     chmod +x "$DUB_LOCATION"/dmd
     chmod +x "$DUB_LOCATION"/dub
     export "PATH=$DUB_LOCATION:$PATH"
@@ -11,15 +11,15 @@ fi
 if [ "$TRAVIS_OS_NAME" = "windows" ]; then
     wget -nv http://downloads.dlang.org/releases/2.x/"$DMD_VERSION"/dmd."$DMD_VERSION".windows.zip 
     echo "dmd downloaded"
-    7z -bso0 -bsp0 x dmd."$DMD_VERSION".windows.zip
+    7z -bso0 -bsp0 x dmd."$DMD_VERSION".windows.zip dmd."$DMD_VERSION"
     echo "dmd extracted"
-    export "PATH=$PWD/dmd2/windows/bin:$PATH"
+    export "PATH=$PWD/dmd.""$DMD_VERSION""/windows/bin:$PATH"
     echo "PATH set"
 fi
 if [ "$TRAVIS_OS_NAME" = "osx" ]; then
     wget -nv http://downloads.dlang.org/releases/2.x/"$DMD_VERSION"/dmd."$DMD_VERSION".osx.tar.xz
-    tar -xf dmd."$DMD_VERSION".osx.tar.xz 1>/dev/null
-    export DUB_LOCATION=$PWD/dmd2/osx/bin
+    tar -xf dmd."$DMD_VERSION".osx.tar.xz 1>/dev/null -C dmd."$DMD_VERSION"
+    export DUB_LOCATION=$PWD/dmd."$DMD_VERSION"/osx/bin
     chmod +x "$DUB_LOCATION"/dmd
     chmod +x "$DUB_LOCATION"/dub
     export "PATH=$DUB_LOCATION:$PATH"
