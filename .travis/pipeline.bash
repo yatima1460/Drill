001293dd76529087354767258a8452e8f7d10240
Federico Santamorena <federico@santamorena.me>
2019-08-14 04:35:02 +0200

    test 2

--- .travis/pipeline.bash
+++ .travis/pipeline.bash
@@ -5,19 +5,19 @@ source .travis/install_d.bash
 source .travis/install_gtk.bash
 
 # Unittests
-dub test -c CLI || travis_terminate 1  &
-dub test -c GTK || travis_terminate 1  &
+dub test -c CLI || exit 1  &
+dub test -c GTK || exit 1  &
 wait
 
 # Builds
 echo $MAIN_VERSION.$TRAVIS_BUILD_NUMBER > TRAVIS_VERSION
-dub build -c CLI -b release-travis || travis_terminate 1 &
-dub build -c GTK -b release-travis || travis_terminate 1 &
+dub build -c CLI -b release-travis || exit 1 &
+dub build -c GTK -b release-travis || exit 1 &
 wait
 
 # Packages
-7z a -tzip Drill-$MAIN_VERSION.$TRAVIS_BUILD_NUMBER-CLI-$TRAVIS_OS_NAME-x86_64.zip $PWD/Build/Drill-GTK-$TRAVIS_OS_NAME-x86_64-release-travis/* || travis_terminate 1 &
-7z a -tzip Drill-$MAIN_VERSION.$TRAVIS_BUILD_NUMBER-GTK-$TRAVIS_OS_NAME-x86_64.zip $PWD/Build/Drill-CLI-$TRAVIS_OS_NAME-x86_64-release-travis/* || travis_terminate 1 &
+7z a -tzip Drill-$MAIN_VERSION.$TRAVIS_BUILD_NUMBER-CLI-$TRAVIS_OS_NAME-x86_64.zip $PWD/Build/Drill-GTK-$TRAVIS_OS_NAME-x86_64-release-travis/* || exit 1 &
+7z a -tzip Drill-$MAIN_VERSION.$TRAVIS_BUILD_NUMBER-GTK-$TRAVIS_OS_NAME-x86_64.zip $PWD/Build/Drill-CLI-$TRAVIS_OS_NAME-x86_64-release-travis/* || exit 1 &
 wait
 
 # if this is a pull request we stop here
