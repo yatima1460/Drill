1a5b13fb913a3d6538c794f811a4bdf7dd8cf760
Federico Santamorena <federico@santamorena.me>
2019-08-14 13:12:40 +0200

    travis fixes

--- .travis/create_appimage.bash
+++ .travis/create_appimage.bash
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sudo apt install -y appstream
+
 mkdir -p Drill.AppDir
-cp -R $PWD/Build/Drill-GTK-linux-x86_64-release-travis/* Drill.AppDir
+cp -R "$PWD"/Build/Drill-GTK-linux-x86_64-release-travis/* Drill.AppDir
 mkdir -p Drill.AppDir/usr/share/metainfo
 mkdir -p Drill.AppDir/usr/share/applications
 cp Assets/GTK-Linux/drill.software.appdata.xml Drill.AppDir/usr/share/metainfo
@@ -13,5 +13,5 @@ chmod +x Drill.AppDir/drill-gtk
 cp Assets/GTK-Linux/drill-gtk.svg Drill.AppDir
 wget https://github.com/AppImage/AppImageKit/releases/download/12/appimagetool-x86_64.AppImage -O appimagetool-x86_64.AppImage
 chmod +x appimagetool-x86_64.AppImage
-export ARCH=x86_64 && ./appimagetool-x86_64.AppImage Drill.AppDir Drill-$MAIN_VERSION.$TRAVIS_BUILD_NUMBER-x86_64.AppImage
-test -f Drill-$MAIN_VERSION.$TRAVIS_BUILD_NUMBER-x86_64.AppImage
+export ARCH=x86_64 && ./appimagetool-x86_64.AppImage Drill.AppDir Drill-"$MAIN_VERSION"."$TRAVIS_BUILD_NUMBER"-x86_64.AppImage
+test -f Drill-"$MAIN_VERSION"."$TRAVIS_BUILD_NUMBER"-x86_64.AppImage
