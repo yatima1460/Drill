34576d689690e38c6042039e35b198dcca6c871a
Federico Santamorena <federico@santamorena.me>
2019-08-14 05:08:00 +0200

    travis test

--- .travis/pipeline.bash
+++ .travis/pipeline.bash
@@ -24,14 +24,29 @@ fi
 # Packages
 7z a -tzip Drill-$MAIN_VERSION.$TRAVIS_BUILD_NUMBER-CLI-$TRAVIS_OS_NAME-x86_64.zip $PWD/Build/Drill-GTK-$TRAVIS_OS_NAME-x86_64-release-travis/* &
 7z a -tzip Drill-$MAIN_VERSION.$TRAVIS_BUILD_NUMBER-GTK-$TRAVIS_OS_NAME-x86_64.zip $PWD/Build/Drill-CLI-$TRAVIS_OS_NAME-x86_64-release-travis/* &
-# TODO: .deb CLI
-# TODO: .deb GTK
-# TODO: .rpm CLI
-# TODO: .rpm GTK
-# TODO: .AppImage GTK
+if [ "$TRAVIS_OS_NAME" = "linux" ]; then
+    source .travis/create_appimage.bash
+    # TODO: .deb CLI
+    # TODO: .deb GTK
+    # TODO: .rpm CLI
+    # TODO: .rpm GTK
+fi
+
 # TODO: installer windows
 wait
 
 # Upload fresh beta packages
+
+# CLI
 curl -T Drill-$MAIN_VERSION.$TRAVIS_BUILD_NUMBER-CLI-$TRAVIS_OS_NAME-x86_64.zip -uyatima1460:$BINTRAY_API_KEY https://api.bintray.com/content/yatima1460/Drill/Portable-Beta/$MAIN_VERSION.$TRAVIS_BUILD_NUMBER/Drill-$MAIN_VERSION.$TRAVIS_BUILD_NUMBER-CLI-$TRAVIS_OS_NAME-x86_64.zip
 curl -X POST -uyatima1460:$BINTRAY_API_KEY https://api.bintray.com/content/yatima1460/Drill/Portable-Beta/$MAIN_VERSION.$TRAVIS_BUILD_NUMBER/publish
+
+# GTK
+curl -T Drill-$MAIN_VERSION.$TRAVIS_BUILD_NUMBER-GTK-$TRAVIS_OS_NAME-x86_64.zip -uyatima1460:$BINTRAY_API_KEY https://api.bintray.com/content/yatima1460/Drill/Portable-Beta/$MAIN_VERSION.$TRAVIS_BUILD_NUMBER/Drill-$MAIN_VERSION.$TRAVIS_BUILD_NUMBER-GTK-$TRAVIS_OS_NAME-x86_64.zip
+curl -X POST -uyatima1460:$BINTRAY_API_KEY https://api.bintray.com/content/yatima1460/Drill/Portable-Beta/$MAIN_VERSION.$TRAVIS_BUILD_NUMBER/publish
+
+# AppImage
+if [ "$TRAVIS_OS_NAME" = "linux" ]; then
+    curl -T Drill-$MAIN_VERSION.$TRAVIS_BUILD_NUMBER-x86_64.AppImage -uyatima1460:$BINTRAY_API_KEY https://api.bintray.com/content/yatima1460/Drill/AppImage-Beta/$MAIN_VERSION.$TRAVIS_BUILD_NUMBER/Drill-$MAIN_VERSION.$TRAVIS_BUILD_NUMBER-x86_64.AppImage
+    curl -X POST -uyatima1460:$BINTRAY_API_KEY https://api.bintray.com/content/yatima1460/Drill/AppImage-Beta/$MAIN_VERSION.$TRAVIS_BUILD_NUMBER/publish
+fi
