b6c06145d234233b7d3771d9e64fe92f60b695c9
Federico Santamorena <federico@santamorena.me>
2019-08-15 13:38:41 +0200

    winapi improvements

--- WinAPI/WinAPI.d
+++ WinAPI/WinAPI.d
@@ -6,79 +6,6 @@ import std.utf;
 
 import core.sys.windows.windows;
 
-const string g_szClassName = "windowwitheditbox";
-
-const int IDC_MAIN_EDIT = 101;
-
-extern (Windows) LRESULT WindowProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) nothrow
-{
-    switch (msg)
-    {
-    case WM_CREATE:
-        {
-            HFONT hfDefault;
-            HWND hEdit;
-
-            hEdit = CreateWindowEx(WS_EX_CLIENTEDGE, "EDIT", "default text",
-                    WS_CHILD | WS_VISIBLE | WS_VSCROLL | WS_HSCROLL | ES_AUTOVSCROLL | ES_AUTOHSCROLL,
-                    0, 0, 100, 100, hwnd, cast(HMENU) IDC_MAIN_EDIT, GetModuleHandle(NULL), NULL);
-
-                    
-            
-            if (hEdit == NULL)
-                MessageBox(hwnd, "Could not create edit box.", "Error", MB_OK | MB_ICONERROR);
-
-            hfDefault = cast(HFONT) GetStockObject(DEFAULT_GUI_FONT);
-            SendMessage(hEdit, WM_SETFONT, cast(WPARAM) hfDefault, MAKELPARAM(FALSE, 0));
-        }
-        break;
-    case WM_SIZE:
-        {
-            HWND hEdit;
-            RECT rcClient;
-
-            GetClientRect(hwnd, &rcClient);
-
-            hEdit = GetDlgItem(hwnd, IDC_MAIN_EDIT);
-            SetWindowPos(hEdit, NULL, 0, 0, rcClient.right, rcClient.bottom, SWP_NOZORDER);
-        }
-        break;
-    case WM_CLOSE:
-        DestroyWindow(hwnd);
-        break;
-    case WM_DESTROY:
-        PostQuitMessage(0);
-        break;
-    default:
-        return DefWindowProc(hwnd, msg, wParam, lParam);
-    }
-    return 0;
-}
-
-extern (Windows) long WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
-        LPSTR lpCmdLine, int iCmdShow)
-{
-    ulong result;
-    void exceptionHandler(Throwable e)
-    {
-        throw e;
-    }
-
-    try
-    {
-        Runtime.initialize();
-        result = myWinMain(hInstance, hPrevInstance, lpCmdLine, iCmdShow);
-        Runtime.terminate();
-    }
-    catch (Throwable o)
-    {
-        MessageBox(null, o.toString().toUTF16z, "Error", MB_OK | MB_ICONEXCLAMATION);
-        result = 0;
-    }
-
-    return result;
-}
-
 RECT rect;
 int width = 1920 / 2;
 int height = 1080 / 2;
@@ -91,49 +18,53 @@ int center_window(HWND parent_window, int width, int height)
     return 0;
 }
 
-ulong myWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+extern (Windows) int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+        PWSTR pCmdLine, int nCmdShow)
 {
-    WNDCLASSEX wc;
+    MSG msg;
     HWND hwnd;
-    MSG Msg;
+    WNDCLASSW wc;
 
-    wc.cbSize = WNDCLASSEX.sizeof;
-    wc.style = 0;
-    wc.lpfnWndProc = &WindowProc;
+    wc.style = CS_HREDRAW | CS_VREDRAW;
     wc.cbClsExtra = 0;
     wc.cbWndExtra = 0;
+    wc.lpszClassName = "Window";
     wc.hInstance = hInstance;
-    wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
-    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
-    wc.hbrBackground = cast(HBRUSH)(COLOR_WINDOW + 1);
+    wc.hbrBackground = GetSysColorBrush(COLOR_3DFACE);
     wc.lpszMenuName = NULL;
-    wc.lpszClassName = cast(wchar*)(g_szClassName);
-    wc.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+    wc.lpfnWndProc = &WndProc;
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
 
-    if (!RegisterClassEx(&wc))
-    {
-        MessageBox(NULL, "Window Registration Failed!", "Error!", MB_ICONEXCLAMATION | MB_OK);
-        return 0;
-    }
+    RegisterClassW(&wc);
 
     center_window(GetDesktopWindow(), width, height);
-
-    hwnd = CreateWindowEx(0, cast(wchar*)(g_szClassName), "Drill",
-            WS_OVERLAPPEDWINDOW, rect.left, rect.top, width, height, null, null, hInstance, null);
-
-    if (hwnd == NULL)
-    {
-        MessageBox(NULL, "Window Creation Failed!", "Error!", MB_ICONEXCLAMATION | MB_OK);
-        return 0;
-    }
+    hwnd = CreateWindowW(wc.lpszClassName, "Drill", WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+            rect.left, rect.top, width, height, NULL, NULL, hInstance, NULL);
 
     ShowWindow(hwnd, nCmdShow);
     UpdateWindow(hwnd);
 
-    while (GetMessage(&Msg, NULL, 0, 0) > 0)
+    while (GetMessage(&msg, NULL, 0, 0))
     {
-        TranslateMessage(&Msg);
-        DispatchMessage(&Msg);
+        DispatchMessage(&msg);
     }
-    return Msg.wParam;
+
+    return cast(int) msg.wParam;
+}
+
+extern (Windows) LRESULT WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) nothrow
+{
+
+    switch (msg)
+    {
+    case WM_DESTROY:
+
+        PostQuitMessage(0);
+        break;
+    default:
+        break;
+    }
+
+    return DefWindowProcW(hwnd, msg, wParam, lParam);
 }
