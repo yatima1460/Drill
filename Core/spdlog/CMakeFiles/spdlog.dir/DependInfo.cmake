b5219ac13287dacbf6735ddc698fda1f1f2f1cfc
Federico Santamorena <federico@santamorena.me>
2020-02-06 22:09:32 +0100

    update dub version

---
+++ Core/spdlog/CMakeFiles/spdlog.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/yatima1460/Desktop/Drill/Core/spdlog/src/async.cpp" "/home/yatima1460/Desktop/Drill/Core/spdlog/CMakeFiles/spdlog.dir/src/async.cpp.o"
+  "/home/yatima1460/Desktop/Drill/Core/spdlog/src/color_sinks.cpp" "/home/yatima1460/Desktop/Drill/Core/spdlog/CMakeFiles/spdlog.dir/src/color_sinks.cpp.o"
+  "/home/yatima1460/Desktop/Drill/Core/spdlog/src/file_sinks.cpp" "/home/yatima1460/Desktop/Drill/Core/spdlog/CMakeFiles/spdlog.dir/src/file_sinks.cpp.o"
+  "/home/yatima1460/Desktop/Drill/Core/spdlog/src/fmt.cpp" "/home/yatima1460/Desktop/Drill/Core/spdlog/CMakeFiles/spdlog.dir/src/fmt.cpp.o"
+  "/home/yatima1460/Desktop/Drill/Core/spdlog/src/spdlog.cpp" "/home/yatima1460/Desktop/Drill/Core/spdlog/CMakeFiles/spdlog.dir/src/spdlog.cpp.o"
+  "/home/yatima1460/Desktop/Drill/Core/spdlog/src/stdout_sinks.cpp" "/home/yatima1460/Desktop/Drill/Core/spdlog/CMakeFiles/spdlog.dir/src/stdout_sinks.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "SPDLOG_COMPILED_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "spdlog/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Core/spdlog/CMakeFiles/spdlog.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+file(REMOVE_RECURSE
+  "libspdlog.a"
+)
