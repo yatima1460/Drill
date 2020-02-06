fd7c40442ac7eb6a87cbb1832547e5e6556b650c
Federico Santamorena <federico@santamorena.me>
2020-02-06 22:09:32 +0100

    update dub version

--- Core/Meta.d
+++ Core/Meta.d
@@ -10,10 +10,11 @@ immutable(string) getDubStringValue(immutable(string) key)
     import std.conv : to;
     return j[key].str;
 }
-immutable(string) VERSION = mixin("getDubStringValue(\"version\")");
+// immutable(string) VERSION = mixin("getDubStringValue(\"version\")");
+immutable(string) VERSION = import("DRILL_VERSION");
 immutable(string) BUILD_TIME  = __TIMESTAMP__;
 immutable(string) AUTHOR_NAME = "Federico Santamorena";
 immutable(string) AUTHOR_URL  = "https://www.linkedin.com/in/yatima1460/";
-immutable(string) GITHUB_URL  = mixin("getDubStringValue(\"homepage\")");
+immutable(string) GITHUB_URL  = getDubStringValue("homepage");
 immutable(string) WEBSITE_URL = "https://www.drill.santamorena.me";
 
