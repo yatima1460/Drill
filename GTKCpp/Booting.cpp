8935d89202b37f54d9dd629118b878b705c5277c
Federico Santamorena <federico@santamorena.me>
2020-02-12 20:22:34 +0100

    more conversion

--- GTKCpp/Booting.cpp
+++ GTKCpp/Booting.cpp
@@ -57,3 +57,29 @@ CONST WindowContext loadUIFromDataFiles(const WindowContext context)
 
     return newContext;
 }
+
+
+void bindEvent(const WindowContext* globalContext, GObject* object, const char* eventName, GCallback callback)
+{
+    assert(object != nullptr);
+    assert(eventName != nullptr);
+    assert(strlen(eventName) != 0);
+    assert(callback != nullptr);
+    gulong result = g_signal_connect(object, eventName, callback, (void*)globalContext);
+    assert(result > 0);
+}
+
+// void bindClickXEvent(const WindowContext* globalContext, GCallback callback)
+// {
+//     // Event when the window is closed using the [X]
+//     assert(context.window != nullptr);
+//     assert(callback != nullptr);
+//     g_signal_connect(G_OBJECT(newContext.window), "destroy", callback, globalContext);
+// }
+
+
+    // assert(context !is null);
+    // assert(context.window !is null);
+    // assert(&check_escape !is null);
+    // assert(app !is null);
+    // context.window.g_signal_connect("key_press_event", &check_escape, context);
--- GTKCpp/Booting.hpp
+++ GTKCpp/Booting.hpp
@@ -4,4 +4,9 @@
 
 
 
-WindowContext loadUIFromDataFiles(const WindowContext wc);
+WindowContext loadUIFromDataFiles(const WindowContext);
+
+/*
+    Utility function to do some sanity checks before binding GTK events
+*/
+void bindEvent(const WindowContext* globalContext, GObject* object, const char* eventName, GCallback callback);
