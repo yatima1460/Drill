d191cd4063fc734904a00a521989821e16ffe9c3
Yatima Santamorena <yatima@santamorena.me>
2021-11-12 01:24:19 +0100

    add breaks to remove CLI compilation warning

--- CLI/CLI.d
+++ CLI/CLI.d
@@ -96,6 +96,7 @@ int main(string[] args)
         case 0:
             writeln("Your operating system does not pass the executable path as first argument");
             exit(-2);
+            break;
             
         // No search string
         case 1:
@@ -105,6 +106,7 @@ int main(string[] args)
             defaultGetoptPrinter("Options:", opt.options);
             import core.stdc.stdlib : exit;
             exit(-1);
+            break;
 
         // Search string provided
         case 2:
