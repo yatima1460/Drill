3461447c58656a2172ebe1a0746ff0171003c744
Yatima Santamorena <yatima@santamorena.me>
2021-11-12 01:28:42 +0100

    remove useless function and formatting

--- CLI/CLI.d
+++ CLI/CLI.d
@@ -1,7 +1,5 @@
-
-
 import core.stdc.stdlib : exit;
-import core.stdc.stdio :printf;
+import core.stdc.stdio : printf;
 
 import std.array : join;
 import std.stdio : writeln, readln;
@@ -16,35 +14,32 @@ import Meta : VERSION, GITHUB_URL;
 // TODO: capture Ctrl-C and close crawlers?
 
 import std.variant : Variant;
-void resultsFoundWithDate(const(FileInfo) result,  void*  userObject)
+
+void resultsFoundWithDate(const(FileInfo) result, void* userObject)
 {
     synchronized
     {
-        writeln(result.dateModifiedString,"\t",result.fullPath);
+        writeln(result.dateModifiedString, "\t", result.fullPath);
     }
 }
 
-
-void resultsFoundWithSize(const(FileInfo) result,  void*  userObject)
+void resultsFoundWithSize(const(FileInfo) result, void* userObject)
 {
     synchronized
     {
-        writeln(result.sizeString,"\t",result.fullPath);
+        writeln(result.sizeString, "\t", result.fullPath);
     }
 }
-
 
 void resultsFoundWithSizeAndDate(const(FileInfo) result, void* userObject)
 {
     synchronized
     {
-        writeln(result.dateModifiedString,"\t",result.sizeString,"\t",result.fullPath);
+        writeln(result.dateModifiedString, "\t", result.sizeString, "\t", result.fullPath);
     }
 }
 
-
-
-void resultsFoundBare(const(FileInfo) result,  void*  userObject)
+void resultsFoundBare(const(FileInfo) result, void* userObject)
 {
     synchronized
     {
@@ -52,74 +47,60 @@ void resultsFoundBare(const(FileInfo) result,  void*  userObject)
     }
 }
 
-
-immutable(string) searchInput()
-{
-    printf("Search for: ");
-    string search = readln();
-    immutable(string) searchString = search[0 .. search.length-1];
-    if (searchString.length == 0)
-        return searchInput();
-    return searchString;
-}
-
-
 int main(string[] args)
 {
     import std.path : dirName, buildNormalizedPath, absolutePath;
     import std.getopt : getopt, defaultGetoptPrinter, config;
 
-    DrillConfig data = loadData(buildPath(absolutePath(dirName(buildNormalizedPath(args[0]))),"Assets"));
+    DrillConfig data = loadData(
+            buildPath(absolutePath(dirName(buildNormalizedPath(args[0]))), "Assets"));
 
     bool date = false;
     bool size = false;
 
-    auto opt = getopt(args,
-        config.bundling,
-        config.passThrough,
-        "date|d", "Show results date", &date,
-        "size|s", "Show results size", &size
-    );
+    auto opt = getopt(args, config.bundling, config.passThrough, "date|d",
+            "Show results date", &date, "size|s", "Show results size", &size);
 
-    if(opt.helpWanted) 
+    if (opt.helpWanted)
     {
-        writeln("Drill CLI v"~VERSION~" - "~GITHUB_URL);
+        writeln("Drill CLI v" ~ VERSION ~ " - " ~ GITHUB_URL);
         writeln("Example use: drill-cli -ds \"foobar\"");
         defaultGetoptPrinter("Options:", opt.options);
         import core.stdc.stdlib : exit;
+
         exit(0);
     }
 
     switch (args.length)
     {
         // What even is POSIX
-        case 0:
-            writeln("Your operating system does not pass the executable path as first argument");
-            exit(-2);
-            break;
-            
+    case 0:
+        writeln("Your operating system does not pass the executable path as first argument");
+        exit(-2);
+        break;
+
         // No search string
-        case 1:
-            writeln("Drill CLI v"~VERSION~" - "~GITHUB_URL);
-            writeln("Pass a string as an argument for Drill to search");
-            writeln("Example use: drill-cli -ds \"foobar\"");
-            defaultGetoptPrinter("Options:", opt.options);
-            import core.stdc.stdlib : exit;
-            exit(-1);
-            break;
+    case 1:
+        writeln("Drill CLI v" ~ VERSION ~ " - " ~ GITHUB_URL);
+        writeln("Pass a string as an argument for Drill to search");
+        writeln("Example use: drill-cli -ds \"foobar\"");
+        defaultGetoptPrinter("Options:", opt.options);
+        import core.stdc.stdlib : exit;
+
+        exit(-1);
+        break;
 
         // Search string provided
-        case 2:
-            auto selectedPrint = (date ?
-                             (size ? &resultsFoundWithSizeAndDate : &resultsFoundWithDate)
-                             :(size ? &resultsFoundWithSize : &resultsFoundBare));
-            auto context = startCrawling(data,args[1],*&selectedPrint,null);
-            break;
+    case 2:
+        auto selectedPrint = (date ? (size ? &resultsFoundWithSizeAndDate
+                : &resultsFoundWithDate) : (size ? &resultsFoundWithSize : &resultsFoundBare));
+        auto context = startCrawling(data, args[1], *&selectedPrint, null);
+        break;
 
         // More unnecessary arguments
-        default:
-            writeln("Oops, you gave more arguments than expected.");
-            exit(-1);
+    default:
+        writeln("Oops, you gave more arguments than expected.");
+        exit(-1);
     }
     return 0;
 }
