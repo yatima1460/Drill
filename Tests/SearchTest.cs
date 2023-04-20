3a7f78839b461d36f138e55cdac85edb25f44cea
Yatima Santamorena <yatima@santamorena.me>
2023-04-21 01:38:02 +0200

    add some tests

---
+++ Tests/SearchTest.cs
@@ -0,0 +1,111 @@
+namespace Tests;
+
+
+
+public class UnitTest1
+{
+    [Fact]
+    public void SimpleStartAndStop()
+    {
+        Search s = new Search("test", (string result) => {
+            Console.WriteLine("SimpleStartAndStop: "+result);
+        });
+        s.Start();
+        s.Stop();
+    }
+
+    [Fact]
+    public void PassInvalidSearchString()
+    {
+        Assert.Throws<ArgumentException>(() => new Search(null, (string result) => {
+            Console.WriteLine("PassInvalidSearchString: "+result);
+        }));
+    }
+
+    
+    [Fact]
+    public void PassInvalidResultsCallback()
+    {
+        Assert.Throws<ArgumentException>(() => new Search("test", null));
+    }
+
+        
+    [Fact]
+    public void PassEmptySearchString()
+    {
+        Assert.Throws<ArgumentException>(() => new Search("", (string result) => {
+            Console.WriteLine("PassEmptySearchString: "+result);
+        }));
+    }
+
+    [Fact(Timeout = 1000)]
+    public void StartTwice()
+    {
+        Search s = new Search("test", (string result) => {
+            Console.WriteLine("StartTwice: "+result);
+        });
+        s.Start();
+        Assert.Throws<InvalidOperationException>(() => s.Start());
+    }
+    
+
+    [Fact]
+    public void StopTwice()
+    {
+        Search s = new Search("test", (string result) => {
+            Console.WriteLine("StopTwice: "+result);
+        });
+        s.Start();
+        s.Stop();
+        Assert.Throws<InvalidOperationException>(() => s.Stop());
+    }
+
+    [Fact]
+    public void StopWithoutStarting()
+    {
+        Search s = new Search("test", (string result) => {
+            Console.WriteLine("StopWithoutStarting: "+result);
+        });
+        Assert.Throws<InvalidOperationException>(() => s.Stop());
+    }
+
+    [Fact]
+    public void WaitWithoutStarting()
+    {
+        Search s = new Search("test", (string result) => {
+            Console.WriteLine("WaitWithoutStarting: "+result);
+        });
+        Assert.Throws<InvalidOperationException>(() => s.Wait());
+    }
+
+    // [Fact(Timeout = 1000)]
+    // public void WaitWithoutStopping()
+    // {
+    //     Search s = new Search("test", (string result) => {
+    //         Console.WriteLine("WaitWithoutStopping: "+result);
+    //     });
+    //     s.Start();
+    //     Assert.Throws<InvalidOperationException>(() => s.Wait());
+    // }
+
+    [Fact]
+    public void PassAllNull()
+    {
+        Assert.Throws<ArgumentException>(() => new Search(null, null));
+    }
+
+
+    [Fact]
+    public void StartAndStopMultipleTimes()
+    {
+        Search s = new Search("test", (string result) => {
+            Console.WriteLine("StartAndStopMultipleTimes: "+result);
+        });
+        s.Start();
+        s.Stop();
+        s.Start();
+        s.Stop();
+        s.Start();
+        s.Stop();
+    }
+}
