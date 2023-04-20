b40e8cdc3697b387243f0f0dac5202c52bf15354
Yatima Santamorena <yatima@santamorena.me>
2023-04-20 19:48:24 +0200

    Add CLI

---
+++ CLI/CLI.cs
@@ -0,0 +1,28 @@
+﻿namespace CLI;
+
+using System.Diagnostics;
+
+
+public class CLI
+{
+    public static void Main(string[] args)
+    {
+        Debug.WriteLine("Drill debug output");
+        if (args.Length == 0)
+        {
+            Console.WriteLine("No arguments provided.");
+            return;
+        }
+
+        Debug.WriteLine("Starting search...");
+        Search search = new Search(args[0], ResultsCallback);
+        search.Start();
+        search.Wait();
+    }
+
+    public static void ResultsCallback(string result)
+    {
+        Console.WriteLine(result);
+    }
+
+}
