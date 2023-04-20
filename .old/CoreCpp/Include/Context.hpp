f71cdcdd161507ef7a90a21b29fa281fed6a6492
Yatima Santamorena <yatima@santamorena.me>
2023-04-20 19:44:34 +0200

    remove old garbage

--- .old/CoreCpp/Include/Context.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <vector>
-
-#include "CrawlerData.hpp"
-
-// Given a list of crawlers will return the active ones
-int ActiveCrawlersCount(const std::vector<CrawlerData> crawlers);
