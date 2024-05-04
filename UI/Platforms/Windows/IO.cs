564932c1336f62b9c28e2b8cd017e49a741fc74a
Yatima Santamorena <yatima@santamorena.me>
2024-05-04 17:17:17 +0200

    fixes for windows

--- UI/Platforms/Windows/IO.cs
+++ UI/Platforms/Windows/IO.cs
@@ -1,41 +1,33 @@
 
 
 
+using System.Diagnostics;
+
 namespace Drill;
 
 public static class IO {
 
-    public static void OpenFile(string path)
+    public static void OpenFile(string FullPath)
     {
-        try
-        {
+        
               Process.Start(new ProcessStartInfo
                 {
                     FileName = "explorer.exe",
                     Arguments = "\"" + FullPath + "\""
                 });
-        }
-        catch (Exception e)
-        {
-            DisplayAlert("Error opening file", "FullPath: " + FullPath + "\n" + e.ToString(), "OK");
-        }
+        
     }
 
 
-    public static void OpenPath(string path)
+    public static void OpenPath(string FullPath)
     {
-        try
-        {
+        
             Process.Start(new ProcessStartInfo
                 {
                     FileName = "explorer.exe",
                     Arguments = string.Format("/select,\"{0}\"", FullPath)
                 });
-        }
-        catch (Exception e)
-        {
-            DisplayAlert("Error opening file", "FullPath: " + FullPath + "\n" + e.ToString(), "OK");
-        }
+        
     }
    
 
