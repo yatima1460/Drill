d21309cc401ace962fba0445449cea619fd3ffbb
Yatima Santamorena <yatima@santamorena.me>
2024-05-07 15:52:42 +0200

    add async

---
+++ Drill/Core/DiskRead.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill.Core
+{
+    internal static class DiskRead
+    {
+
+
+        private static FileInfo[] _safeGetFilesInDirectory(DirectoryInfo directoryInfo)
+        {
+            try
+            {
+                return directoryInfo.GetFiles("*", SearchOption.TopDirectoryOnly);
+            }
+            catch (Exception e)
+            {
+                return [];
+            }
+        }
+
+
+        private static DirectoryInfo[] _safeGetDirectoriesInDirectory(DirectoryInfo directoryInfo)
+        {
+            try
+            {
+                return directoryInfo.GetDirectories("*", SearchOption.TopDirectoryOnly);
+            }
+            catch (Exception e)
+            {
+                return [];
+            }
+        }
+
+        public static async Task<FileInfo[]> GetFilesInDirectoryAsync(DirectoryInfo directoryInfo)
+        {
+            // Run the synchronous file operation in a background task
+            return await Task.Run(() => _safeGetFilesInDirectory(directoryInfo));
+        }
+
+        public static async Task<DirectoryInfo[]> GetDirectoriesInDirectoryAsync(DirectoryInfo directoryInfo)
+        {
+            // Run the synchronous file operation in a background task
+            return await Task.Run(() => _safeGetDirectoriesInDirectory(directoryInfo));
+        }
+    }
+}
--- Drill/Core/Search.cs
+++ Drill/Core/Search.cs
@@ -1,6 +1,7 @@
 ﻿
 
 
+using Microsoft.UI.Xaml.Controls;
 using System.Collections.Concurrent;
 using System.Diagnostics;
 
@@ -18,15 +19,15 @@ namespace Drill.Core
 
         static Search()
         {
-            using var stream =  FileSystem.OpenAppPackageFileAsync("words_alpha.txt").Result;
+            using var stream = FileSystem.OpenAppPackageFileAsync("words_alpha.txt").Result;
             using var reader = new StreamReader(stream);
 
             var contents = reader.ReadToEnd();
-            
+
             foreach (var item in contents.Split("\r\n"))
             {
                 if (item.Length > 4)
-                dict.Add(item);
+                    dict.Add(item);
             }
 
             // done
@@ -42,7 +43,7 @@ namespace Drill.Core
 
         public delegate void FatalErrorCallback(Exception e);
 
-        
+
 
         private static Task? scan;
 
@@ -50,8 +51,8 @@ namespace Drill.Core
         {
             try
             {
-                  // THIS IS HEAVY CALL WIN32 CACHE IT
-                 string UserName = Environment.UserName;
+                // THIS IS HEAVY CALL WIN32 CACHE IT
+                string UserName = Environment.UserName;
 
                 if (scan != null)
                 {
@@ -137,140 +138,33 @@ namespace Drill.Core
 
 
                 _stopRequested = false;
-                scan = new Task(() =>
+                scan = new Task( async () =>
                 {
-
-                    try
+                    while (_stopRequested == false && directoriesToExplore.Count != 0)
                     {
+                        DirectoryInfo rootFolderInfo = directoriesToExplore.PopHighestPriority();
 
-
-                        while (_stopRequested == false && directoriesToExplore.Count != 0)
+                        CrawlFilesInDirectory(rootFolderInfo, searchString);
+                        List<Tuple<DirectoryInfo, SearchPriority>> newFindings = await CrawlDirectoriesInDirectory(rootFolderInfo, searchString);
+                        
+                        // TODO: move this in queue Add method
+                        foreach (var item in newFindings)
                         {
-                            DirectoryInfo rootFolderInfo = directoriesToExplore.PopHighestPriority();
-
-                            
-
-                            try
+                            switch (item.Item2)
                             {
-                                // Directory.GetFileSystemEntries()
-                                FileInfo[] subs = rootFolderInfo.GetFiles("*", SearchOption.TopDirectoryOnly);
-
-                                foreach (FileInfo file in subs)
-                                {
-                                    if (_stopRequested) break;
-                                    if (StringUtils.TokenMatching(searchString, file.Name))
-                                    {
-                                        // Better to create the DrillResult on the backend than the UI thread to not stall it
-                                        DrillResult drillResult = new()
-                                        {
-                                            Name = file.Name,
-                                            FullPath = file.FullName,
-                                            Path = rootFolderInfo.FullName,
-                                            Date = file.LastWriteTime.ToString("F"),
-                                            Size = StringUtils.GetHumanReadableSize(file),
-                                            Icon = ExtensionIcon.GetIcon(file.Extension.ToLower())
-                                        };
-
-                                        // this may stall for a sec
-                                        ParallelResults.Enqueue(drillResult);
-                                    }
-                                }
-
-                                DirectoryInfo[] di = rootFolderInfo.GetDirectories("*", SearchOption.TopDirectoryOnly);
-                                foreach (DirectoryInfo sub in di)
-                                {
-                                    if (_stopRequested) break;
-                                    // TODO move to Platforms
-                                    if (
-                                        sub.FullName == $"/Users/{UserName}/Pictures/Photos Library.photoslibrary" ||
-                                        sub.FullName == $"/Users/{UserName}/Library/Calendars" ||
-                                        sub.FullName == $"/Users/{UserName}/Library/Reminders" ||
-                                        sub.FullName == $"/Users/{UserName}/Library/Contacts"
-                                        )
-                                    {
-                                        continue;
-                                    }
-
-                                    if (StringUtils.TokenMatching(searchString, sub.Name))
-                                    {
-                                        // Better to create the DrillResult on the backend than the UI thread to not stall it
-                                        DrillResult drillResult = new()
-                                        {
-                                            Name = sub.Name,
-                                            FullPath = sub.FullName,
-                                            Path = rootFolderInfo.FullName,
-                                            Date = sub.LastWriteTime.ToString("F"),
-                                            Size = "",
-                                            // TODO: different icon for .app on Mac
-                                            Icon = "📁"
-                                        };
-
-                                        // this may stall for a sec
-                                        ParallelResults.Enqueue(drillResult);
-                                    }
-
-
-                                        switch (GetDirectoryPriority(sub, searchString))
-                                    {
-                                        case SearchPriority.Low:
-                                            directoriesToExplore.AddLowPriority(sub);
-                                            break;
-                                        case SearchPriority.Normal:
-                                            directoriesToExplore.AddNormalPriority(sub);
-                                            break;
-                                        case SearchPriority.High:
-                                            directoriesToExplore.AddHighPriority(sub);
-                                            break;
-                                    }
-
-                                    
-
-
-
-
-
-
-                                    //List<DirectoryInfo> directoryInfosPrioritized = new List<DirectoryInfo>();
-
-                                    //foreach (DirectoryInfo item in directoriesToExplore)
-                                    //{
-                                    //    if (sub.Name.StartsWith(".") ||
-                                    //        (sub.Attributes & FileAttributes.Hidden) == FileAttributes.Hidden ||
-                                    //        (sub.Attributes & FileAttributes.System) == FileAttributes.System ||
-                                    //         (sub.Attributes & FileAttributes.Temporary) == FileAttributes.Temporary ||
-                                    //         sub.FullName.StartsWith("C:\\Windows")
-                                    //        )
-                                    //    {
-                                    //        directoriesToExplore.Add(sub);
-                                    //    }
-                                    //    else
-                                    //    {
-                                    //        directoriesToExplore.Insert(0, sub);
-                                    //    }
-
-                                    //}
-                                    //directoriesToExplore = directoryInfosPrioritized;
-
-
-                                }
-                            }
-                            catch (Exception e)
-                            {
-#if DEBUG
-                        Debug.Print(e.Message);
-#endif
-                                continue;
+                                case SearchPriority.Low:
+                                    directoriesToExplore.AddLowPriority(item.Item1);
+                                    break;
+                                case SearchPriority.Normal:
+                                    directoriesToExplore.AddNormalPriority(item.Item1);
+                                    break;
+                                case SearchPriority.High:
+                                    directoriesToExplore.AddHighPriority(item.Item1);
+                                    break;
                             }
                         }
 
-                    }
-                    catch (Exception e)
-                    {
-                        _stopRequested = true;
-#if DEBUG
-                Debug.Print(e.Message);
-#endif
-                        //errorHandler(e);
+
                     }
                 });
                 scan.Start();
@@ -286,16 +180,89 @@ namespace Drill.Core
             }
         }
 
+        private static async Task<List<Tuple<DirectoryInfo, SearchPriority>>> CrawlDirectoriesInDirectory(DirectoryInfo rootFolderInfo, string searchString)
+        {
+            List<Tuple<DirectoryInfo, SearchPriority>> newFindings = [];
+
+            DirectoryInfo[] di = await DiskRead.GetDirectoriesInDirectoryAsync(rootFolderInfo);
+            foreach (DirectoryInfo sub in di)
+            {
+                if (_stopRequested) break;
+                // TODO move to Platforms
+                if (
+                    sub.FullName == $"/Users/{UserName}/Pictures/Photos Library.photoslibrary" ||
+                    sub.FullName == $"/Users/{UserName}/Library/Calendars" ||
+                    sub.FullName == $"/Users/{UserName}/Library/Reminders" ||
+                    sub.FullName == $"/Users/{UserName}/Library/Contacts"
+                    )
+                {
+                    continue;
+                }
+
+                if (StringUtils.TokenMatching(searchString, sub.Name))
+                {
+                    // Better to create the DrillResult on the backend than the UI thread to not stall it
+                    DrillResult drillResult = new()
+                    {
+                        Name = sub.Name,
+                        FullPath = sub.FullName,
+                        Path = rootFolderInfo.FullName,
+                        Date = sub.LastWriteTime.ToString("F"),
+                        Size = "",
+                        // TODO: different icon for .app on Mac
+                        Icon = "📁"
+                    };
+
+                    // this may stall for a sec
+                    ParallelResults.Enqueue(drillResult);
+                }
+
+                newFindings.Add(new Tuple<DirectoryInfo, SearchPriority>(sub, GetDirectoryPriority(sub, searchString)));
+
+
+
+            }
+
+            return newFindings;
+        }
+
+        private static async void CrawlFilesInDirectory(DirectoryInfo rootFolderInfo, string searchString)
+        {
+            // Directory.GetFileSystemEntries()
+            FileInfo[] subs = await DiskRead.GetFilesInDirectoryAsync(rootFolderInfo);
+
+            foreach (FileInfo file in subs)
+            {
+                if (_stopRequested) break;
+                if (StringUtils.TokenMatching(searchString, file.Name))
+                {
+                    // Better to create the DrillResult on the backend than the UI thread to not stall it
+                    DrillResult drillResult = new()
+                    {
+                        Name = file.Name,
+                        FullPath = file.FullName,
+                        Path = rootFolderInfo.FullName,
+                        Date = file.LastWriteTime.ToString("F"),
+                        Size = StringUtils.GetHumanReadableSize(file),
+                        Icon = ExtensionIcon.GetIcon(file.Extension.ToLower())
+                    };
+
+                    // this may stall for a sec
+                    ParallelResults.Enqueue(drillResult);
+                }
+            }
+        }
+
         private static SearchPriority GetDirectoryPriority(DirectoryInfo sub, string searchString)
         {
             if (
-            sub.Name.StartsWith(".") 
+            sub.Name.StartsWith(".")
             || (sub.Attributes & FileAttributes.Hidden) == FileAttributes.Hidden ||
             (sub.Attributes & FileAttributes.System) == FileAttributes.System ||
             (sub.Attributes & FileAttributes.Temporary) == FileAttributes.Temporary ||
             sub.FullName.StartsWith("C:\\Windows")
             || sub.FullName.StartsWith($"C:\\Users\\{UserName}\\AppData")
-            || (sub.Parent != null &&  sub.Parent.FullName == "C:\\")
+            || (sub.Parent != null && sub.Parent.FullName == "C:\\")
             || (sub.Attributes & FileAttributes.ReparsePoint) == FileAttributes.ReparsePoint
             )
             {
@@ -337,12 +304,12 @@ namespace Drill.Core
         {
             _stopRequested = true;
             if (scan != null)
-            {  
+            {
                 scan.Wait();
                 scan.Dispose();
                 scan = null;
             }
-            ParallelResults.Clear();   
+            ParallelResults.Clear();
         }
 
         public static List<DrillResult> PopResults(int count)
