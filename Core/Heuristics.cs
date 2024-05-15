d95533e16f33e33191914f276ab9d7e74a58f2fc
Yatima Santamorena <yatima@santamorena.me>
2024-05-15 15:00:57 +0200

    cleanup + folderprofile publish

--- Core/Heuristics.cs
+++ Core/Heuristics.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿
 using System.Collections.Immutable;
-using System.Diagnostics;
-using System.Linq;
+
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Linq;
-using static System.Net.Mime.MediaTypeNames;
+
 
 namespace Drill.Core
 {
--- Core/StringUtils.cs
+++ Core/StringUtils.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Concurrent;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace Drill.Core
 {
     public static class StringUtils
